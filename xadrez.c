b43cdbfeb6f7d4c15bdc045fe24091da9157a1c3
JonasBrunoBenicio <jonas__bruno@hotmail.com>
2025-04-24 21:50:47 +0000

    Movimentação de peças Finalizado

--- xadrez.c
+++ xadrez.c
@@ -5,5 +5,41 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    printf("Desafio Xadrez!\n");
-}
+        // Nível Novato - Movimentação das Peças Xadrez
+
+        int bispoDiagonal;
+        int torreDireita = 1;
+        int rainhaEsquerda = 1;
+    
+        // Movimentação do Bispo
+        // Utilizando estrutura de repetição for.
+        for (bispoDiagonal = 1; bispoDiagonal <= 5; bispoDiagonal++)
+        {
+            printf("Bispo andou %d casas para diagonal superior direita\n", bispoDiagonal); //imprime movimento da peça
+        }
+    
+        printf("\n");
+    
+        // Movimentação da Torre
+        // Utilizando estrutura de repetição while
+        while (torreDireita <= 5)
+        {
+            printf("Torre moveu %d casas para direita\n", torreDireita);
+            torreDireita++; //imprime movimento da peça
+        }
+    
+        printf("\n");
+    
+        // Movimentação da Rainha
+        // Utilizando estrurura de repetição Do-While
+        do
+        {   
+            printf("Rainha moveu %d casas para esquerda\n", rainhaEsquerda);
+            rainhaEsquerda++; //imprime movimento da peça
+        } while (rainhaEsquerda <= 8);
+        
+        printf("\n");
+    
+     
+        return 0;
+    }
